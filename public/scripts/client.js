d1f7c1303db51af5fdca9e984142880b0bdde4c5
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-21 18:46:43 +0000

    added function to create tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,47 @@
-/* unable to timeAgo.js please talk to Joe Lee. */
-$(document).ready(() => {
-  $(".need_to_be_rendered").each((index, element) => {
-    let currentTime = new Date();
-    let elemTime = Date.parse(element.attributes.datetime.value);
-    let dateTime = currentTime.getTime() - elemTime;
-    let daysAgo = dateTime / (1000 * 3600 * 24);
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
-    console.log(element)
-    element.innerText = `${Math.round(daysAgo)} days ago`;
+const createTweetElement = (tweet) => {
+  return $(`
+    <article class="tweet" id = custom>
+      <div class="tweet-header">
+        <div class="Name">
+          <img src="${tweet.user.avatars}" alt="user-image" />
+          ${tweet.user.name}
+        </div>
+        <div class="user-id">${tweet.user.handle}</div>
+      </div>
+      <div class="tweet-main">${tweet.content.text}</div>
+      <div class="tweet-footer">
+        <div
+          class="need_to_be_rendered"
+          datetime="${tweet.created_at}"
+        ></div>
+        <div class="tweet-icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </div>
+    </article>`
+  )
+};
+
+const $tweet = createTweetElement(tweetData);
+
+$(document).ready(() => {
+  $(".tweet-container").append($tweet)
+  $(".need_to_be_rendered").each((index, element) => {
+    element.innerText = timeago.format(element.attributes.datetime.value);
 
   });
 });
