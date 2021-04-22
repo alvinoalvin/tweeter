b5a379b63ce5acd940d2a8f3cf0e96c73b1f00b2
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-22 20:47:14 +0000

    2 column layout for desktop, forgot to commit, but switched to scss

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
   });
 
   $(".right").on("click", function(event) {
-    $(".new-tweet").slideToggle("slow", (() => {
-      if ($(".new-tweet").css("display") === "block") {
-        $(".write-tweet-arrow").html(`<i class="fas fa-angle-double-up"></i>`);
+    $("#new-tweet").slideToggle("slow", (() => {
+      if ($("#new-tweet").css("display") === "block") {
+        $("#write-tweet-arrow").html(`<i class="fas fa-angle-double-up"></i>`);
         window.scrollTo(0, 0)
         $("#tweet-text").focus();
       }
       else {
-        $(".write-tweet-arrow").html(`<i class="fas fa-angle-double-down"></i>`);
+        $("#write-tweet-arrow").html(`<i class="fas fa-angle-double-down"></i>`);
       }
     }));
   });
